209d817056645c0ec5fbfab63746750ab8b0266c
Mathieu Lefrancois <mathieu.lefrancois@florizontechnologies.com>
2026-07-03 19:26:31 -0400

    feat: render Studio Element+ Librairie tab as preview tiles

--- tests/ScadaBuilderV2.Tests/StudioElementPlusLibraryTabContractTests.cs
+++ tests/ScadaBuilderV2.Tests/StudioElementPlusLibraryTabContractTests.cs
@@ -55,6 +55,31 @@ public sealed class StudioElementPlusLibraryTabContractTests
         StringAssert.Contains(xaml, "x:Name=\"NameTextBox\"");
     }
 
+    [TestMethod]
+    public void MainWindowXamlRendersLibraryItemsAsPreviewTiles()
+    {
+        var xaml = ReadProjectFile("src", "ScadaBuilderV2.ElementStudio.App", "MainWindow.xaml");
+
+        StringAssert.Contains(xaml, "xmlns:local=\"clr-namespace:ScadaBuilderV2.ElementStudio.App\"");
+        StringAssert.Contains(xaml, "<WrapPanel/>");
+        StringAssert.Contains(xaml, "local:HtmlPreviewControl");
+        StringAssert.Contains(xaml, "Markup=\"{Binding PreviewMarkup}\"");
+        StringAssert.Contains(xaml, "Text=\"{Binding IconText}\"");
+        StringAssert.Contains(xaml, "Text=\"{Binding FileName}\"");
+        StringAssert.Contains(xaml, "ToolTip=\"{Binding DetailText}\"");
+    }
+
+    [TestMethod]
+    public void HtmlPreviewControlExistsInElementStudioApp()
+    {
+        var code = ReadProjectFile("src", "ScadaBuilderV2.ElementStudio.App", "HtmlPreviewControl.cs");
+
+        StringAssert.Contains(code, "namespace ScadaBuilderV2.ElementStudio.App;");
+        StringAssert.Contains(code, "public sealed class HtmlPreviewControl : UserControl");
+        StringAssert.Contains(code, "public static readonly DependencyProperty MarkupProperty");
+        StringAssert.Contains(code, "browser.NavigateToString(");
+    }
+
     private static string ReadProjectFile(params string[] parts)
     {
         var directory = new DirectoryInfo(AppContext.BaseDirectory);
